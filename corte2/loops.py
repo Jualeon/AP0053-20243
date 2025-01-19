6f1a3d42682f16b214e5befd5a3e274fe1fda3ca
Jualeon <151046378+Jualeon@users.noreply.github.com>
2025-01-18 20:26:21 -0500

    Update loops.py

--- corte2/loops.py
+++ corte2/loops.py
@@ -64,5 +64,54 @@ for letra in cadena:
     time.sleep(1)
 
 #primos#
+import time
+inicio = time.time()
+
+for i in range(0,31):
+    conta = 0
+    for n in range(1, i+1):
+        residue = i%n
+        if residue == 0:
+            conta = conta + 1
+
+        if conta == 2:
+            print(f'{i} Es un primo')
+
+fin = time.time()
+print("t = ", (fin - inicio)*1000) 
+
+#primos2#
+a = 1
+value = input('Ingrese un valor: ')
+value = int(value)
+
+while a == 1:
+    for i in range(1, value + 1):
+        conta = 0
+        for n in range(1, i+1):
+            if i % n == 0:
+            #if residue == 0:
+                conta += 1    
+    if conta == 2:
+        print(f'{i} Es un primo')
+        print("\n")
+    else:
+        print(f'{i} No es un primo')
+        print("\n")
+
+        print('Do you want to continue?. Press 1 to do that')
+
+    a = input()
+    a = int(a)
+
+    if a != 1:
+        break
+
+    value = input('Ingrese un valor: ')
+    value = int(value)
+
+#True or false#
+my_cool_variable = 7 + 8 == 11
+print(my_cool_variable)
 
 
